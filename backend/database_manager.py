9d3797d5a531a35871c998ced6083362446ad010
pkzadeh <pkzadeh@gmail.com>
2025-06-26 16:08:16 +0700

    Refactored vector_tables to database_manager, added a constant connection as FastAPI will be hosting the lifecycle of the application.

--- backend/database_manager.py
+++ backend/database_manager.py
@@ -1,50 +1,86 @@
 import sqlite3
 import logging
-from vector_models import Vector
+import datetime
+from .vector_models import Vector
+from typing import Optional, List, Dict
+
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class DBManager : 
-    def __init__(self, db_connection_information):
-        self.db_connection_information = db_connection_information
-        logging.debug(f"DBManager initialized with connection info: {self.db_connection_information}")
+    def __init__(self, db_file_name : str):
+        self.db_file_name = db_file_name
+        self.conn : Optional[sqlite3.Connection] = None
+        self.cursor : Optional[sqlite3.Cursor] = None
+        logging.debug(f"DBManager instance created for {self.db_file_name}")
 
-    def _get_connection(self):
-        logging.debug("Establishing database connection.")
-        return sqlite3.connect(self.db_connection_information, timeout=10)
-    
-    def execute_query(self, query, params=()):
-        logging.debug(f"Executing query: {query}")
+   
+   # Method to create one continual connection with the database, that we use on launch of the fastAPI lifecycle
+    def connect_to_database(self):
         try:
-            with self._get_connection() as conn:
-                cursor = conn.cursor()
-                cursor.execute(query, params)
-                conn.commit()
-                logging.info("Query executed successfully.")
-                return cursor
+            self.conn = sqlite3.connect(self.db_file_name, timeout=10)
+            self.conn.row_factory = sqlite3.Row
+            self.cursor = self.conn.cursor()
+            logging.info(f"Connected to database: {self.db_file_name}")
         except sqlite3.Error as e:
-            logging.error(f"An error occurred while executing the query: {e}")
+            logging.error(f"Database connection error: {e}")
             raise
+
+    def close_connection(self):
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+            self.cursor = None
+            logging.info("Database connection closed")
+        else:
+            logging.warning("Attempt to close database unsuccessful, as no active connection found")
+    
+    def execute_query(self, query : str, params : tuple =()):
+        if not self.conn or not self.cursor:
+            logging.error("NO CURRENT ACTIVE DATABASE CONNECTION. CANNOT EXECUTE QUERY")
+            raise sqlite3.Error("Ensure connect_to_database is called for active DB connection")
+        
+        logging.debug(f"Executing query: {query} with params : {params} ")
+        try:
+            self.cursor.execute(query, params)
+            self.conn.commit()
+            logging.info(f"Query : {query}, executed")
+            return self.cursor
+        except sqlite3.Error as e:
+            logging.error("Error occured as query was executing")
+            self.conn.rollback()
+            raise
+
+
+
+
+
+    
+        
+        
+
 
 # will call execute query to insert the vector data into two different tables in our database. 
     def insert_vector_data(self, vector_object):
+        if not self.conn or not self.cursor:
+                logging.error("No active connection, cannot insert vector data")
+                return None
+        
         logging.debug(f"Inserting vector data: {vector_object}")
+        
         try:
-            with self._get_connection() as conn:
-                cursor = conn.cursor()
-
                 # Insert into vectors metadata table
-                cursor.execute('''
+                self.cursor.execute('''
                     INSERT INTO vectors_metadata (length, color, shape, creation_timestamp)
                     VALUES (?, ?, ?, CURRENT_TIMESTAMP)
                 ''', (vector_object.length, vector_object.color, vector_object.shape))
                 
-                vector_id = cursor.lastrowid
+                vector_id = self.cursor.lastrowid
                 processed_dimensions = vector_object.dimensions.copy()
                 if len(processed_dimensions) < 5:
-                    processed_dimensions += [0] * (5 - len(processed_dimensions))  # Pad with zeros if less than 5 dimensions
-                processed_dimensions = processed_dimensions[:5]  # Ensure we only take the first 5 digits
+                    processed_dimensions += [0] * (5 - len(processed_dimensions))  
+                processed_dimensions = processed_dimensions[:5]  
 
 
                 logging.info(f"Inserted vector metadata with ID: {vector_id}")
@@ -55,33 +91,37 @@ class DBManager :
                 for index, value in enumerate(processed_dimensions):
                     dimensions_to_insert.append((vector_id, index, value))
 
-                cursor.executemany('''
+                self.cursor.executemany('''
                         INSERT INTO vector_dimensions (vector_id, dimension_index, dimension_value)
                         VALUES (?, ?, ?)
                  ''', dimensions_to_insert)
                 
                 logging.info("Inserted vector dimensions successfully.")
-                conn.commit()
+                self.conn.commit()
                 return vector_id
         except sqlite3.Error as e:
             logging.error(f"An error occurred while inserting vector data: {e}")
+            self.conn.rollback()
             return None
             
 
             # Low level method to fetch data from the database using a query and parameters.
     def fetch_query(self, query, params=()):
-        logging.debug(f"Fetching data with query: {query}")
+        if not self.conn or not self.cursor:
+            logging.error("No active connection, cannot fetch query")
+            raise sqlite3.Error("No active database connection. call connect_to_database() .")
+        
+        logging.debug(f"Fetching data with query: {query} with params: {params}")
+
         try:
-            with self._get_connection() as conn:
-                conn.row_factory = sqlite3.Row
-                cursor = conn.cursor()
-                cursor.execute(query, params)
-                results = cursor.fetchall()
-                logging.info("Data fetched successfully.")
-                return [dict(row) for row in results]  # Convert rows to dictionaries for easier access
+            
+            self.cursor.execute(query, params) 
+            results = self.cursor.fetchall() 
+            logging.info("Data fetched successfully.")
+            return [dict(row) for row in results] 
         except sqlite3.Error as e:
             logging.error(f"An error occurred while fetching data: {e}")
-            raise
+            raise 
 
 
        # Method to retrieve vector metadata by ID from vectors_metadata table.     
@@ -130,12 +170,13 @@ class DBManager :
 
     def create_tables(self):
         logging.debug("Attempting to create tables in the database.")
-        try: 
-            with self._get_connection() as conn:
-                cursor = conn.cursor()
-
-                # Table for main vector metadata
-                cursor.execute('''
+        if not self.conn or not self.cursor:
+                logging.error("No active database connection, cannot create tables")
+                raise sqlite3.Error("Cannot create tables, connect_to_database() must be called to ensure connection is active")
+        
+        try:
+             # Table for main vector metadata
+                self.cursor.execute('''
                     CREATE TABLE IF NOT EXISTS vectors_metadata (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         length INTEGER NOT NULL,
@@ -148,7 +189,7 @@ class DBManager :
                 logging.info("Table for vectors metadata created successfully.")
 
                 # Table for individual vector dimensions
-                cursor.execute('''
+                self.cursor.execute('''
                     CREATE TABLE IF NOT EXISTS vector_dimensions (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         vector_id INTEGER NOT NULL,
@@ -160,18 +201,18 @@ class DBManager :
                 logging.info("Table for vector dimensions created successfully.")
 
                 # Create an index to speed up searches on specific dimensions
-                cursor.execute('''
+                self.cursor.execute('''
                     CREATE INDEX IF NOT EXISTS idx_vector_dimensions_lookup
                     ON vector_dimensions (dimension_index, dimension_value)
                 ''')
                 logging.info("Index for vector dimensions created successfully.")
 
-                cursor.execute('''
+                self.cursor.execute('''
                                CREATE INDEX IF NOT EXISTS idx_vectors_color
                                  ON vectors_metadata (color)
                                  ''')
                 logging.info("Index for vector colors created successfully.")
-                cursor.execute('''
+                self.cursor.execute('''
                                CREATE INDEX IF NOT EXISTS idx_vectors_shape
                                  ON vectors_metadata (shape)
                                  ''')
@@ -179,6 +220,7 @@ class DBManager :
 
         except sqlite3.Error as e:
             logging.error(f"An error occurred while creating tables: {e}")
+            self.conn.rollback()
             raise
         logging.debug("All tables created successfully.")
 
